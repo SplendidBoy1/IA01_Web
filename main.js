132fcf9b4cdfb0f89d6e567673107e2e7e1b20d9
Datatat <lexuandat6062@gmail.com>
2024-10-26 20:03:35 +0700

    all newx box

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 $(".move_symbol").on("mousedown", function () {
     $(".drag_news").sortable(
         {   
-            cursor: "move",
+            cursor: "n-resize",
             helper: "clone",
             revert: true,
             axis: "y",
@@ -17,6 +17,29 @@ $(".move_symbol").on("mousedown", function () {
 })
 
 $(".move_symbol").on("mouseenter", function () {
-    $(".move_symbol").css("cursor", "move")
+    $(".move_symbol").css("cursor", "n-resize")
 })
 
+$(".more_content_symbol").on("mousedown", function () {
+    if ($(this.parentNode.parentNode.children[1]).css("display") == "none"){
+        $(this.parentNode.parentNode.children[0]).css("background-color", "orange")
+        $(this.parentNode.parentNode.children[0]).css("color", "white")
+        $(this.parentNode.parentNode.children[0]).css("opacity", 1)
+        $(this.parentNode.parentNode.children[1]).css("display", "inline")
+        $(this).text('⏵')
+        // $(this).text
+    }
+    else{
+        $(this.parentNode.parentNode.children[1]).css("display", "none")
+        $(this).text('🠣')
+        $(this.parentNode.parentNode.children[0]).css("background-color", "#FFD3A7")
+        $(this.parentNode.parentNode.children[0]).css("color", "black")
+        $(this.parentNode.parentNode.children[0]).css("opacity", 0.7)
+    }
+})
+
+$(".more_content_symbol").on("mouseenter", function () {
+    $(".more_content_symbol").css("cursor", "grab")
+})
+
+////////////////////

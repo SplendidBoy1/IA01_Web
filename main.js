85f16371ef6e8e5bbb27e7d5017dd70c13b26bf5
Datatat <lexuandat6062@gmail.com>
2024-10-27 01:31:10 +0700

    add some lorem

--- main.js
+++ main.js
@@ -52,9 +52,96 @@ $(".more_content_symbol").on("mouseenter", function () {
 $(".toggle").hide()
 
 $(".button_image").on("click", function(){
-    console.log("asdf")
+    console.log("Dat")
     $(".toggle").toggle()
     // $(".toggle").css(" visibility", "visible")
+})
+
+$("#bold_check").on("click", function(){
+    $(".toggle").toggle()
+    let flag = $("#bold_check").is(":checked")
+    if (flag){
+        $(".show_all_sample").css("font-weight", "bold")
+    }
+    else{
+        $(".show_all_sample").css("font-weight", "")
+    }
+})
+
+$("#italic_check").on("click", function(){
+    $(".toggle").toggle()
+    let flag = $("#italic_check").is(":checked")
+    if (flag){
+        $(".show_all_sample").css("font-style", "italic")
+    }
+    else{
+        $(".show_all_sample").css("font-style", "")
+    }
+    // console.log("asdf")
+})
+
+$("#underline_check").on("click", function(){
+    $(".toggle").toggle()
+    let flag = $("#underline_check").is(":checked")
+    if (flag){
+        $(".show_all_sample").css("text-decoration", "underline")
+    }
+    else{
+        $(".show_all_sample").css("text-decoration", "")
+    }
+    // console.log("asdf")
+})
+
+$("#background_color").on("input", function(){
+    // $(".toggle").toggle()
+    let value_background = this.value
+    $(".show_all_sample").css("background-color", value_background)
+})
+
+$(".color_input").on("input", function(){
+    // $(".toggle").toggle()
+    let value_background = this.value
+    $(".show_all_sample").css("color", value_background)
+})
+
+function clear_hightlight(){
+    $(".text_lorem span").each(function(){
+        // console.log($(this).text())
+        let value = $(this).text()
+        // $(this).remove()
+        $(this).replaceWith(value)
+      });
+}
+
+$("#highlight").on("click", function(){
+    clear_hightlight();
+    let regex_text = $(".process_text_input").val()
+    let data = $(".text_lorem").html()
+    // console.log(data)
+    if (regex_text != ""){
+        let regex = new RegExp(regex_text, "g");
+
+        let hightlight = data.match(regex);
+        let matches = [...data.matchAll(regex)];
+        for (let i = 0; i < hightlight.length; i++){
+            data = data.replaceAt(matches[i].index, '<span class=\"show_all_sample\">' + hightlight[i] + '</span>')
+            console.log("Asdf")
+        }
+        console.log(matches.length)
+        console.log(hightlight)
+        // let hightlight = data.replace(regex, '<span class=\"show_all_sample\">' + data. + '</span>')
+        // console.log(hightlight)
+        $(".text_lorem").html(data)
+    }
+    // let newText = text.replace(re, `<mark>${searched}</mark>`);
+})
+
+$("#delete").on("click", function(){
+    console.log("asdf")
+})
+
+$("#reset").on("click", function(){
+    console.log("asdf")
 })
 
 // $('.dialog').dialog({
